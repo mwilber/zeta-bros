564d7603aa47b99f720239726254d366a9e58ff3
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-06-29 15:55:43 -0400

    Set up platforms

--- src/scenes/DemoScene.js
+++ src/scenes/DemoScene.js
@@ -9,11 +9,24 @@ export class DemoScene extends Phaser.Scene {
 	}
 
 	preload() {
-        
+        this.load.image('background', 'assets/images/background.png');
+        this.load.image('platform', 'assets/images/platform.png');
+        this.load.spritesheet('zeta', 
+            'assets/images/zeta_spritesheet.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
     }
 
     create() {
+        this.add.image(400, 300, 'background');
 
+        let platforms = this.physics.add.staticGroup();
+
+        platforms.create(400, 568, 'platform').setScale(2).refreshBody();
+
+        platforms.create(600, 400, 'platform');
+        platforms.create(50, 250, 'platform');
+        platforms.create(750, 220, 'platform');
     }
 
     update() {
