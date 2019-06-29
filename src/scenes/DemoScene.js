3e1445bf92ae8bb5d6528ecee9d3f1e412a038fa
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-06-29 16:31:39 -0400

    Set up basic game from phaser demo

--- src/scenes/DemoScene.js
+++ src/scenes/DemoScene.js
@@ -22,14 +22,67 @@ export class DemoScene extends Phaser.Scene {
 
         let platforms = this.physics.add.staticGroup();
 
-        platforms.create(400, 568, 'platform').setScale(2).refreshBody();
+        this.platforms.create(400, 568, 'platform').setScale(2).refreshBody();
 
-        platforms.create(600, 400, 'platform');
-        platforms.create(50, 250, 'platform');
-        platforms.create(750, 220, 'platform');
+        this.platforms.create(600, 400, 'platform');
+        this.platforms.create(50, 250, 'platform');
+        this.platforms.create(750, 220, 'platform');
+
+        this.player = this.physics.add.sprite(100, 450, 'zeta');
+
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('zeta', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'zeta', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('zeta', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.physics.add.collider(this.player, platforms);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
+
+        if (this.cursors.left.isDown)
+        {
+            this.player.setVelocityX(-160);
+
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play('right', true);
+        }
+        else
+        {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play('turn');
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down)
+        {
+            this.player.setVelocityY(-330);
+        }
 
     }
 
