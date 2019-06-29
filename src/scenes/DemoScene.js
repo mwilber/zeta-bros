ff6b52812837314cbe064a161e7e34d75019d899
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-06-29 13:26:52 -0400

    Set up phaser

---
+++ src/scenes/DemoScene.js
@@ -0,0 +1,23 @@
+
+import Phaser from 'phaser';
+
+export class DemoScene extends Phaser.Scene {
+    constructor() {
+		super({
+			key: 'DemoScene'
+        });
+	}
+
+	preload() {
+        
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+
+}
