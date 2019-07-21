63957c4a7802c11f4d8c4d3233c9a73103da1c91
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-07-21 16:50:52 -0400

    Add some initial sfx

--- src/scenes/IntroScene.js
+++ src/scenes/IntroScene.js
@@ -10,15 +10,21 @@ export class IntroScene extends Phaser.Scene {
 
 	preload() {
         this.load.image('splash', 'assets/images/intro_splash.png');
+        this.load.audio('aud_start_game', 'assets/audio/start_game.wav');
     }
 
     create() {
         // Add the background image
         this.splash = this.add.image(400, 300, 'splash');
+        this.audStartGame = this.sound.add('aud_start_game');
+
+        
         
         this.splash.setInteractive().on('pointerdown', () => { 
             console.log('start game'); 
+            this.audStartGame.play();
             this.scene.start('Level1');
+            
         });
     }
 }
