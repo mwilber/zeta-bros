acc15597c1535841ff2fc2189c84b7ae37e9f7bb
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-07-28 16:51:49 -0400

    remove second door. add original platform configuration

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -14,22 +14,15 @@ export class Level1 extends GameScene{
 
         let platforms = super.createPlatforms();
 
-        platforms.create(100, 180, 'platform');
-        platforms.create(500, 180, 'platform');
+        platforms.create(100, 135, 'platform');
+        platforms.create(700, 135, 'platform');
+        
+        platforms.create(400, 270, 'platform');
 
-        platforms.create(700, 360, 'platform');
-        platforms.create(300, 360, 'platform');
+        platforms.create(50, 405, 'platform');
+        platforms.create(750, 405, 'platform');
 
         return platforms;
-    }
-
-    createDoors(){
-        let door = super.createDoors();
-
-        // Add an exit door
-        door.create(100, 132, 'door', 3);
-
-        return door;
     }
 
     create() {
