4dd198cc09a0b948d83f78d12eed79665f7fec60
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-08-03 21:53:46 -0400

    add player sprite

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -9,15 +9,30 @@ export class GameScene extends Phaser.Scene {
 
 	preload() {
         this.load.image('background', 'assets/images/background.png');
+        this.load.spritesheet('zeta', 
+            'assets/images/zeta_spritesheet_alpha.png',
+            { frameWidth: 40, frameHeight: 66 }
+        );
     }
 
     create() {
         // Add the background image
         this.add.image(400, 300, 'background');
+
+        this.player         = this.createPlayer();
     }
 
     update() {
 
+    }
+
+    createPlayer() {
+        let player = this.physics.add.sprite(400, 450, 'zeta');
+
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+        
+        return player;
     }
     
 }
